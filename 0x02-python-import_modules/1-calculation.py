f5eabf22236c322725b66acd36429ce1234fe1c5
ogamodey <ogamodeymoses@gmail.com>
2023-08-16 17:33:18 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,12 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    sum_result = add(a, b)
+    diff_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+
+    print(f"sum: {sum_result}")
+    print(f"Difference: {diff_result}")
+    print(f"Multiple: {mul_result}")
+    print(f"Division: {div_Result}")
